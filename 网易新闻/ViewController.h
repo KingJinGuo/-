887b4b8f08d9d3b5de27d1c2283de7ebaa0cd439
King <595316088@qq.com>
2017-06-10 17:03:43 +0800

    Initial Commit

---
+++ 网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易新闻
+//
+//  Created by King on 2017/6/10.
+//  Copyright © 2017年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 网易新闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易新闻
+//
+//  Created by King on 2017/6/10.
+//  Copyright © 2017年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
